f9a265d48bb7726bf8ac2ac5f5a5009bf0dc84bd
Sergey Volkov <serzhvolkov03@gmail.com>
2023-08-20 10:38:31 +0300

    feat: add create customer

--- src/api/customer/createCustomer.tsx
+++ src/api/customer/createCustomer.tsx
@@ -1,4 +1,4 @@
-import { MyCustomerSignin } from "@commercetools/platform-sdk";
+import { CustomerDraft, MyCustomerSignin } from "@commercetools/platform-sdk";
 import { apiRoot } from "../client/createClient";
 import { apiRootPassword } from "../client/passwordFlow";
 
@@ -6,24 +6,21 @@ export const getProject = () => {
   return apiRoot.get().execute();
 };
 
-// const createCustomer = () => {
-//   return apiRoot
-//     .customers()
-//     .post({
-//       body: {
-//         email: "sdk@example.com",
-//         password: "examplePassword",
-//       },
-//     })
-//     .execute();
-// };
-
-// // Create the customer and output the Customer ID
-// createCustomer()
-//   .then(({ body }) => {
-//     console.log(body.customer.id);
-//   })
-//   .catch(console.error);
+export const createCustomer = (dataForRegitration: CustomerDraft) => {
+  try{
+    return apiRoot
+    .customers()
+    .post({
+      body: {
+        ...dataForRegitration
+      },
+    })
+    .execute();
+  } catch(e){
+    console.log(e)
+  }
+  
+};
 
 export const signInCustomer = async ({ email, password }: MyCustomerSignin) => {
   return await apiRootPassword
@@ -38,14 +35,3 @@ export const signInCustomer = async ({ email, password }: MyCustomerSignin) => {
     .execute();
 };
 
-export const createCustomer = ({ email, password }: MyCustomerSignin) => {
-  return apiRootPassword
-    .customers()
-    .post({
-      body: {
-        email,
-        password,
-      },
-    })
-    .execute();
-};
