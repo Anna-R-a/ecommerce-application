8057d808ba36c14c15afe1f4db29f426f389d2b4
Anna-R-a <ratnikova_a.m@mail.ru>
2023-08-22 21:26:45 +0300

    feat: add tests to client function and registration page

--- src/__tests__/NavbarTest.tsx
+++ src/__tests__/NavbarTest.tsx
@@ -1,15 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { Nav } from "../components/navBar/NavBar";
-import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 
+
 test("should show error message when all the fields are not entered", async () => {
-  render(
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act (() =>{ render(
     <BrowserRouter>
       <Nav />
     </BrowserRouter>,
-  );
-  const buttonElement = screen.getByText("Home");
-  expect(buttonElement).toBeInTheDocument();
-  userEvent.click(buttonElement);
+  )
 });
+  const buttonElement = screen.getByText("Home");
+  await waitFor(() => {
+  expect(buttonElement).toBeInTheDocument();
+  })
+  fireEvent.click(screen.getByRole('link'), {
+    name: 'Home',
+  });
+}, 10000);
