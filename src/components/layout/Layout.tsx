be68290cb74c854ccef84bf2dc6b10e7148ff22b
Anna-R-a <ratnikova_a.m@mail.ru>
2023-09-04 00:11:35 +0300

    feat: add breadcrumps

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
 import "./Layout.css";
+import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 
 export const PageLayout = () => {
   return (
     <>
       <Header />
+      <Breadcrumbs />
       <main className="main">
         <div className="wrapper">
           <Outlet />
