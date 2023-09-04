599306e3a6f84ecb45140a4435682d807bf3e7bd
Nastassia Krasutskaya <krasnastassia@gmail.com>
2023-09-04 09:43:57 +0300

    refactor: edit code by eslint

--- src/pages/catalog/Catalog.tsx
+++ src/pages/catalog/Catalog.tsx
@@ -233,7 +233,7 @@ const CatalogPage: React.FC = () => {
 
   const handlerFilter = (
     nameFilter: string,
-    checkedValues: CheckboxValueType[]
+    checkedValues: CheckboxValueType[],
   ) => {
     console.log("nameFilter", nameFilter);
     checkedValues.length === 0
