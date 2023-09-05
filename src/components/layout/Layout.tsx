a9a8011e7cced587140f1416993cf482b4ea323c
Anna <118760957+Anna-R-a@users.noreply.github.com>
2023-09-05 07:54:49 +0300

    fix: update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,6 @@ import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import "./Layout.css";
-import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 
 export const PageLayout = () => {
   return (
