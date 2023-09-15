12c3e0e7e4ae440fc957fbb2e7acebe6f36ba801
Sergey Volkov <serzhvolkov03@gmail.com>
2023-09-15 21:32:54 +0300

    feat: implement changing quantity product and remove product from cart

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -145,6 +145,62 @@ export const addProductToCart = async (productId: string) => {
     .execute();
 };
 
+export const removeProductFromCart = async (lineItemId: string) => {
+  const tokenLoggedClient = getTokenClient();
+  const tokenClient = tokenLoggedClient ? tokenLoggedClient : apiRootAnonymous;
+
+  const activeCart = await getActiveCart();
+  const cartId = activeCart.body.id;
+  const cartVersion = activeCart.body.version;
+
+  return tokenClient
+    .me()
+    .carts()
+    .withId({ ID: cartId })
+    .post({
+      body: {
+        version: cartVersion,
+        actions: [
+          {
+            action: "removeLineItem",
+            lineItemId: lineItemId,
+          },
+        ],
+      },
+    })
+    .execute();
+};
+
+export const changeQuantityProductInCart = async (
+  productId: string,
+  quantity: number,
+) => {
+  const tokenLoggedClient = getTokenClient();
+  const tokenClient = tokenLoggedClient ? tokenLoggedClient : apiRootAnonymous;
+
+  const activeCart = await getActiveCart();
+  const cartId = activeCart.body.id;
+  const cartVersion = activeCart.body.version;
+
+  return tokenClient
+    .me()
+    .carts()
+    .withId({ ID: cartId })
+    .post({
+      body: {
+        version: cartVersion,
+        actions: [
+          {
+            action: "changeLineItemQuantity",
+            lineItemId: productId,
+            quantity: quantity,
+          },
+        ],
+      },
+    })
+    .execute();
+};
+
 export const getProductDetails = async (childPathArgs: { key: string }) => {
   return apiRoot.products().withKey(childPathArgs).get().execute();
 };
--- src/components/cart-list/CartList.tsx
+++ src/components/cart-list/CartList.tsx
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import { Button, Space, Table } from "antd";
 import type { ColumnsType } from "antd/es/table";
 import { LineItem } from "@commercetools/platform-sdk";
-import { getActiveCart } from "../../api/api";
+import {
+  changeQuantityProductInCart,
+  getActiveCart,
+  removeProductFromCart,
+} from "../../api/api";
 import "./cart-list.css";
 
 interface DataType {
@@ -13,50 +17,6 @@ interface DataType {
   price: string;
   totalPrice: string;
 }
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Image",
-    dataIndex: "image",
-    key: "image",
-    render: (_, record) => {
-      return (
-        <img className="cart-image" src={record.image} alt={record.name}></img>
-      );
-    },
-  },
-  {
-    title: "Count",
-    dataIndex: "count",
-    key: "count",
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-    key: "price",
-  },
-  {
-    title: "Total price",
-    key: "totalPrice",
-    dataIndex: "totalPrice",
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: (_, record) => (
-      <Space size="small">
-        <Button>+</Button>
-        <Button>-</Button>
-        <Button>delete</Button>
-      </Space>
-    ),
-  },
-];
 
 function mapToDataType(data: LineItem[]) {
   const result: DataType[] = [];
@@ -78,6 +38,79 @@ function mapToDataType(data: LineItem[]) {
 const CartList = () => {
   const [productsList, setProductList] = useState<LineItem[]>([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [version, setVersion] = useState(0);
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      render: (_, record) => {
+        return (
+          <img
+            className="cart-image"
+            src={record.image}
+            alt={record.name}
+          ></img>
+        );
+      },
+    },
+    {
+      title: "Count",
+      dataIndex: "count",
+      key: "count",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Total price",
+      key: "totalPrice",
+      dataIndex: "totalPrice",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => {
+        const increseCount = () => {
+          changeQuantityProductInCart(record.key, record.count + 1).then(() => {
+            setVersion((prev) => prev + 1);
+          });
+        };
+
+        const decreseCount = () => {
+          if (record.count > 1) {
+            changeQuantityProductInCart(record.key, record.count - 1).then(
+              () => {
+                setVersion((prev) => prev + 1);
+              },
+            );
+          }
+        };
+
+        const removeProduct = () => {
+          removeProductFromCart(record.key).then(() => {
+            setVersion((prev) => prev + 1);
+          });;
+        };
+
+        return (
+          <Space size="small">
+            <Button onClick={increseCount}>+</Button>
+            <Button onClick={decreseCount}>-</Button>
+            <Button onClick={removeProduct}>delete</Button>
+          </Space>
+        );
+      },
+    },
+  ];
 
   React.useEffect(() => {
     if (
@@ -85,19 +118,25 @@ const CartList = () => {
       localStorage.getItem("cart-customer")
     ) {
       getActiveCart().then((res) => {
-        setTotalPrice(res.body.totalPrice.centAmount)
+        setTotalPrice(res.body.totalPrice.centAmount);
         setProductList(res.body.lineItems);
       });
     }
-  }, []);
+  }, [version]);
 
-  console.log(productsList);
-
-  return <Table columns={columns} dataSource={mapToDataType(productsList)} footer={()=>{
-    return (
-      <p style={{fontSize: 18 }}>Total Sum: {(totalPrice/100).toFixed(2).toString()} $</p>
-    )
-  }}/>;
+  return (
+    <Table
+      columns={columns}
+      dataSource={mapToDataType(productsList)}
+      footer={() => {
+        return (
+          <p style={{ fontSize: 18 }}>
+            Total Sum: {(totalPrice / 100).toFixed(2).toString()} $
+          </p>
+        );
+      }}
+    />
+  );
 };
 
 export default CartList;
